9fc8a54fa0938695d5c7d20514c6ca6e6da5aa7a
izzman12 <izzman12@gmail.com>
2025-07-19 15:38:59 -0400

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from 'react';
+import BillingForm from './BillingForm';
+
+export default function App() {
+  return (
+    <div style={{ backgroundColor: '#e5e7eb', minHeight: '100vh', padding: '2rem' }}>
+      <BillingForm />
+    </div>
+  );
+}
